a8268ffbccfba9bce39bb5a5c42ba913f1e5b37b
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-01-07 22:16:37 -0500

    Added data and populated the tableview cell

--- accelerometron/Controller/ListTableViewController.swift
+++ accelerometron/Controller/ListTableViewController.swift
@@ -22,10 +22,15 @@ class ListTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //A number of cells
+        return imageArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //return a configured cell with image and name 
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "parallaxCell", for: indexPath) as? ParallaxCell else { return UITableViewCell() }
+        cell.configureCell(withImage: imageArray[indexPath.row], andDescription: nameArray[indexPath.row])
+        
+        return cell
     }
 
 }
---
+++ accelerometron/Images+Names.swift
@@ -0,0 +1,20 @@
+//
+//  Images+Names.swift
+//  accelerometron
+//
+//  Created by Ricardo Herrera Petit on 1/7/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+var camera = UIImage(named: "1")!
+var city = UIImage(named: "2")!
+var animals = UIImage(named: "3")!
+var flowers = UIImage(named: "4")!
+var stand = UIImage(named: "5")!
+var urban = UIImage(named: "6")!
+
+var imageArray = [camera, city, animals, flowers, stand, urban]
+var nameArray = ["CAMERAS", "CITIES", "ANIMALS", "FLOWERS", "STANDS", "URBAN" ]
+
